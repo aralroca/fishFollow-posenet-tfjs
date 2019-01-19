3b1c0f337c5c9d10a3249b0f35d7b48f10a79ed4
Aral Roca Gomez <aral-rg@hotmail.com>
2019-01-19 13:14:14 +0100

    Updates

---
+++ precache-manifest.8e213e5afd96dde77a9bbeb00bf4b32d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08a3d4e84b6c6fe069b",
+    "url": "/fishFollow-posenet-tfjs/static/js/runtime~main.d08a3d4e.js"
+  },
+  {
+    "revision": "36714bb5c110034f6f5c",
+    "url": "/fishFollow-posenet-tfjs/static/js/main.36714bb5.chunk.js"
+  },
+  {
+    "revision": "5cd84bb10ad43de26d0d",
+    "url": "/fishFollow-posenet-tfjs/static/js/1.5cd84bb1.chunk.js"
+  },
+  {
+    "revision": "36714bb5c110034f6f5c",
+    "url": "/fishFollow-posenet-tfjs/static/css/main.d0af1e3a.chunk.css"
+  },
+  {
+    "revision": "f580bbf3099514db8c1d2c61dd96f3b6",
+    "url": "/fishFollow-posenet-tfjs/index.html"
+  }
+];
